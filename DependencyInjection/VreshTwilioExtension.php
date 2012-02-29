8fc5c5296e667f46d575554f128d0b90679947bf
Frido Koch <frido@vresh.net>
2012-02-29 10:38:51 +0100

    * Fixed wrong markdown syntax in README

--- DependencyInjection/VreshTwilioExtension.php
+++ DependencyInjection/VreshTwilioExtension.php
@@ -1,4 +1,12 @@
 <?php
+/*
+* This file is part of the VreshTwilio package.
+*
+* (c) Frido Koch <frido@vresh.net
+*
+* For the full copyright and license information, please view the LICENSE
+* file that was distributed with this source code.
+*/
 namespace Vresh\TwilioBundle\DependencyInjection;
 
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
--- VreshTwilioBundle.php
+++ VreshTwilioBundle.php
@@ -1,9 +1,8 @@
 <?php
-
 /*
 * This file is part of the VreshTwilio package.
 *
-* (c) Frido Koch aka Vreshware <https://github.com/Vreshware>
+* (c) Frido Koch <frido@vresh.net
 *
 * For the full copyright and license information, please view the LICENSE
 * file that was distributed with this source code.
